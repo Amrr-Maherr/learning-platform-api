32ce35d5a7e69fe43856cb01c894e5d2115e7ebe
Amr Maher Ali <amrr.maherr24@gmail.com>
2026-08-15 00:35:10 +0300

    Add users routes to the Express app

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ const express = require("express");
 const morgan = require("morgan");
 const { main } = require("./config/db");
 const coursesRoutes = require("./routes/courses.routes");
+const usersRoutes = require("./routes/users.routes");
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(morgan("dev"));
 app.use(express.json());
 app.use("/api/v1/courses", coursesRoutes);
+app.use("/api/v1/users", usersRoutes);
 main()
   .then(() => {
     app.listen(port, () => {
