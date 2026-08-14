4dd12fcc603f41d397f4147865255154746143f5
Amr Maher Ali <amrr.maherr24@gmail.com>
2026-08-14 20:29:46 +0300

    Implement course routes for CRUD operations

--- routes/courses.routes.js
+++ routes/courses.routes.js
@@ -1,2 +1,28 @@
 const express = require("express");
+
+const {
+  getAllCourses,
+  getCourseById,
+  createCourse,
+  updateCourse,
+  deleteCourse,
+} = require("../controllers/courses.controller");
+
 const coursesRoutes = express.Router();
+
+// Get all courses
+coursesRoutes.get("/", getAllCourses);
+
+// Get single course
+coursesRoutes.get("/:id", getCourseById);
+
+// Create new course
+coursesRoutes.post("/", createCourse);
+
+// Update course
+coursesRoutes.patch("/:id", updateCourse);
+
+// Delete course
+coursesRoutes.delete("/:id", deleteCourse);
+
+module.exports = coursesRoutes;
