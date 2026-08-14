05540d9308bf88c5bb7e729c5b74a71d59234707
Amr Maher Ali <amrr.maherr24@gmail.com>
2026-08-14 20:40:25 +0300

    Refactor database connection setup for improved structure and clarity

--- config/db.js
+++ config/db.js
@@ -1,23 +1,22 @@
 const { MongoClient } = require("mongodb");
-// or as an es module:
-// import { MongoClient } from 'mongodb'
 
-// Connection URL
 const url = process.env.DATABASE_URL;
-const client = new MongoClient(url);
-
-// Database Name
 const dbName = process.env.DATABASE_NAME;
 
+const client = new MongoClient(url);
+
+const db = {};
+
 async function main() {
-  // Use connect method to connect to the server
   await client.connect();
-  const db = client.db(dbName);
-  const users_collection = db.collection("users");
-  const courses_collection = db.collection("courses");
+  const database = client.db(dbName);
+  db.database = database;
+  db.users_collection = database.collection("users");
+  db.courses_collection = database.collection("courses");
   return "Connected successfully to server";
 }
 
-main().then(console.log).catch(console.error);
-
-module.exports = main;
+module.exports = {
+  main,
+  db,
+};
