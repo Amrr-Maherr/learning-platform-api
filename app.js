4cff383e8934e0ab5e63a7ffb14226cb7f5b2058
Amr Maher Ali <amrr.maherr24@gmail.com>
2026-08-14 20:40:30 +0300

    Refactor app.js to improve middleware setup and route handling

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 require("dotenv").config();
 const express = require("express");
-const main = require("./config/db");
+const morgan = require("morgan");
+const { main } = require("./config/db");
+const coursesRoutes = require("./routes/courses.routes");
 const app = express();
 const port = process.env.PORT || 3000;
-main();
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+app.use(morgan("dev"));
+app.use("/api/v1/courses", coursesRoutes);
+main()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Example app listening on port ${port}`);
+    });
+  })
+  .catch(console.error);
