ecbbb41885ab29d34ebb27a42c9bb520e11cc502
Amr Maher Ali <amrr.maherr24@gmail.com>
2026-08-14 19:53:31 +0300

    Set up environment configuration and add initial route files for courses and users

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
+require("dotenv").config();
 const express = require("express");
+const main = require("./config/db");
 const app = express();
-const port = 3000;
-
+const port = process.env.PORT || 3000;
+main();
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
